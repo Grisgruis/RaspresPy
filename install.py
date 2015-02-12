5fbb8e5e410402218eab802f7abda746ffc6e252
ChrisLaptop <cdejong1989@gmail.com>
2015-02-12 14:56:06 +0100

    update impress

--- install.py
+++ install.py
@@ -1,4 +1,5 @@
 import os
 
-os.system("sudo apt-get install impress")
-os.system("run.py")
+os.system("sudo apt-get update")
+os.system("sudo apt-get install libreoffice-impress")
+os.system("/home/pi/RaspresPy/run.py")
